a58582271b7f092cf9b4e898ca85f658973145ca
Simon Nickel <simonnickel@googlemail.com>
2024-09-13 23:30:29 +0200

    Add example

--- SwiftUImacOSTabViewContentBehindToolbar/ContentView.swift
+++ SwiftUImacOSTabViewContentBehindToolbar/ContentView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	
+	@State private var selection: String = "A"
+	
+	var body: some View {
+		TabView(selection: $selection) {
+			
+			tab(title: "A")
+			tab(title: "B")
+			tab(title: "C")
+
+		}
+		.tabViewStyle(.sidebarAdaptable)
+	}
+	
+	@TabContentBuilder<String>
+	func tab(title: String) -> some TabContent<String> {
+		Tab(value: title, role: nil) {
+			NavigationStack {
+				ScrollView {
+					VStack(alignment: .leading) {
+						Text(title)
+							.padding(20)
+							.background(.orange)
+						Spacer(minLength: 999)
+					}
+					.frame(maxWidth: .infinity, alignment: .leading)
+				}
+			}
+		} label: {
+			Text(title)
+		}
+	}
 }
 
 #Preview {
