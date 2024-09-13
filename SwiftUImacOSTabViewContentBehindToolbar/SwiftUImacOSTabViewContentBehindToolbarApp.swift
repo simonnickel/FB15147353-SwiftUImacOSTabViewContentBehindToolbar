b671ea8f2893918f5ae565fadb8827795d415f3d
Simon Nickel <simonnickel@googlemail.com>
2024-09-13 23:29:36 +0200

    Initial Commit

---
+++ SwiftUImacOSTabViewContentBehindToolbar/SwiftUImacOSTabViewContentBehindToolbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUImacOSTabViewContentBehindToolbarApp.swift
+//  SwiftUImacOSTabViewContentBehindToolbar
+//
+//  Created by Simon Nickel on 13.09.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUImacOSTabViewContentBehindToolbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
